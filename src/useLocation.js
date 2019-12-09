8e39e91ddc14d6ae39ad19f04bae2b1744d0bc15
Will Cook <hello@doubleucook.com>
2019-12-09 16:59:10 +1300

    added logging for testing

--- src/useLocation.js
+++ src/useLocation.js
@@ -10,6 +10,10 @@ export default () => {
       (err) => setErrorMessage(err.message))
   }, []) // empty array causes this function to run once for the entire lifecycle of this component
 
+  console.log('lat: ', lat)
+  if(errorMessage.length > 0) {
+    console.log('errorMessage: ', errorMessage)
+  }
   // return { lat: lat , errorMessage: errorMessage}
   return [lat, errorMessage]
 }
